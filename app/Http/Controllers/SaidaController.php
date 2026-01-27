c892a7e039f91ddc501cdf17059b1c13161e0307
EllySantos13 <elly.santosgoncalves00@gmail.com>
2026-01-27 13:05:53 -0300

    Crud 4

---
+++ app/Http/Controllers/SaidaController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Saida;
+use Illuminate\Http\Request;
+
+class SaidaController extends Controller
+{
+    public function index()
+    {
+        $saida = Saida::all();
+        return response()->json($saida);
+    }
+
+    public function store(Request $request)
+    {
+        $saida = Saida::create([
+            'id_produto'=> $request-> id_produto,
+            'id_cliente'=> $request-> id_cliente,
+            'quantidade'=> $request-> quantidade
+        ]); 
+        return response()->json($saida);
+    }
+}
